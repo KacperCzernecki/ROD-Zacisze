200e7a76f752f39c595e2014e6ba4ea664df5b78
Kacper Czernecki <walentyczer@gmail.com>
2026-05-05 14:44:38 +0200

    Updated post fetching logic, added primitive pagination

--- app/lib/user/readPost.ts
+++ app/lib/user/readPost.ts
@@ -1,18 +1,21 @@
+import { PostgrestError } from "@supabase/supabase-js";
 import { createClient } from "../supabase/client";
+import { matchesGlob } from "path";
 
-export async function readPost() {
+export async function readPost(page: number) {
   const supabase = createClient();
+  const range = 10;
+  const start = (page - 1) * range;
+  const end = start + range - 1;
 
   try {
-    const { data, error } = await supabase
+    const { data, error, count } = await supabase
       .from("posts")
-      .select("*, post_images(image_url)");
-    if (error) {
-      return "Nie można odczytać postu. Błąd bazy danych";
-    } else {
-      return data;
-    }
+      .select("*, post_images(image_url)", { count: "exact" })
+      .range(start, end);
+
+    return { data, error, count };
   } catch (error) {
-    console.log(error);
+    return { data: null, error: error as PostgrestError };
   }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { readPost } from "./lib/user/readPost";
+import { PostgrestError } from "@supabase/supabase-js";
 import PostCard from "./ui/posts/PostCard";
 
 type Post = {
@@ -13,28 +14,49 @@ type Post = {
 };
 
 export default function Home() {
-  const [data, setData] = useState<Post[] | string | undefined>([]);
+  const [data, setData] = useState<Post[] | undefined | null>([]);
+  const [error, setError] = useState<PostgrestError | null>(null);
+  const [page, setPage] = useState<number>(1);
+  const [count, setCount] = useState<number>(0);
   useEffect(() => {
     const fetch = async () => {
-      const result = await readPost();
-      setData(result);
+      const result = await readPost(page);
+
+      if (result.error) {
+        setError(result.error);
+      }
+      setData(result.data);
+      setCount(result.count || 0);
     };
     fetch();
-  }, []);
+  }, [page]);
+
+  const numOfPages = useMemo(() => {
+    return Math.ceil(count / 10);
+  }, [count]);
   return (
     <div>
-      {data?.map((post: Post) => {
-        return (
-          <PostCard
-            key={post.id}
-            type={post.type}
-            title={post.title}
-            content={post.content}
-            images={post.post_images}
-            created_at={post.created_at}
-          />
-        );
-      })}
+      <div className="flex flex-col gap-10 justify-center items-center">
+        {data?.map((post: Post) => {
+          return (
+            <PostCard
+              key={post.id}
+              type={post.type}
+              title={post.title}
+              content={post.content}
+              images={post.post_images}
+              created_at={post.created_at}
+            />
+          );
+        })}
+      </div>
+      <div>
+        {Array.from({ length: numOfPages }, (_, i) => i + 1).map((pageNum) => (
+          <button key={pageNum} onClick={() => setPage(pageNum)}>
+            {pageNum}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
